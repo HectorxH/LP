fa4889cef98400b7641a86e9f4dd76d53a63018d
nekokamu <spaletcano@gmail.com>
2019-09-16 14:18:18 -0700

    float sum()

--- C/funciones.c
+++ C/funciones.c
@@ -7,3 +7,13 @@ typedef struct dato data;
 list* map(list* l, data (*f)(dato)){
     return l;
 }
+
+float sum(struct list* a){
+  int i;
+  float total = 0;
+  int len = lenght(struct list* a);
+  for (i=0; i<len; i++){
+    total += at(a, i);
+  }
+  return total;
+}
