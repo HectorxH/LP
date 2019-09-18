2fd842f865e60883e815eefa5facd38d0e831953
HectorxH <hectorplj@gmail.com>
2019-09-18 15:22:56 -0700

    Minor fix in funciones.h

--- C/funciones.h
+++ C/funciones.h
@@ -1,23 +1,6 @@
 #ifndef funciones
 #define funciones
 
-struct dato{
-    void* contenido;
-    char tipo;
-};
-
-struct node{
-    struct dato info;
-    struct node* next;
-};
-
-struct list{
-    struct node* actual;
-    struct node* head;
-    struct node* tail;
-    int length;
-};
-
 typedef struct list list;
 typedef struct node node;
 typedef struct dato dato;
