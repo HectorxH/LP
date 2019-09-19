9ed0b13144729cfe270930b20920c5d1f8843476
nekokamu <spaletcano@gmail.com>
2019-09-19 10:08:27 -0700

    main.c, main.out

--- C/main.c
+++ C/main.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "lista.h"
 #include "funciones.h"
 
 typedef struct list list;
 
 int main(){
+    dato dato1;
+    dato1.tipo = 'i';
+    dato1.contenido = (void*)malloc(sizeof(int));
+    *((int*)dato1.contenido) = 20;
+
+    list l;
+    init(&l);
+    append(&l, dato1);
+    print(&l);
+    clear(&l);
+
     printf("Nice");
-    printf("xd2");
-    printf("lol");
-    printf("listo");
     return 0;
 }
